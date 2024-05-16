21e1c4c2fe2dff35786ed9be146c45bf9b007335
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-16 20:23:18 -0300

    chore: Update form component to include required fields

--- src/lib/componets/form.svelte
+++ src/lib/componets/form.svelte
@@ -1,27 +1,30 @@
 <script lang="ts">
       import { onMount } from 'svelte';
-      let data = {
+      import type { WaterQuality } from '$lib/types';
+      import { store, addItem } from '$lib/store/store';
+
+      let data: WaterQuality = {
         date: '',
         time: '',
         location: '',
         tap: 0,
         freeChlorine: 0
       };
-
+      $: console.log($store);
 </script>
 
 <form >
     <label for="date">Dia</label>
-    <input type="date" id="date" name="date" class="input" bind:value={data.date} placeholder="Input">
+    <input type="date" id="date" name="date" class="input" bind:value={data.date} placeholder="Input" required>
     <label for="time">Hora</label>
-    <input type="time" name="time" id="time" class="input" bind:value={data.time}>
+    <input type="time" name="time" id="time" class="input" bind:value={data.time} required>
     <label for="Location">Sector</label>
-    <input type="text" id="location" name="location" class="input" bind:value={data.location}>
+    <input type="text" id="location" name="location" class="input" bind:value={data.location} required>
     <label for="grifo">Grifo</label>
-    <input type="number" name="tap" id="tap" class="input" bind:value={data.tap}>
+    <input type="number" name="tap" id="tap" class="input" bind:value={data.tap} required>
     <label for="freeChlorine">Cloro libre (ppm)</label>
-    <input type="number" name="freeChlorine" id="freeChlorine" class="input" bind:value={data.freeChlorine}>
-    <button type="submit" class="btn variant-filled">Guardar</button>
+    <input type="number" name="freeChlorine" id="freeChlorine" class="input" bind:value={data.freeChlorine} min="0" max="2.50" step="0.01" required>
+    <button on:click={()=> addItem(data)} class="btn variant-filled">Guardar</button>
 </form>
 
 
