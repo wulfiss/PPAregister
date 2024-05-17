9f27fd1825a58b787208aa421ee7b137e734a90c
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-17 20:48:27 -0300

    feat: Add open modal functionality to Chlorine page

--- src/routes/chlorine/+page.svelte
+++ src/routes/chlorine/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 import TableChlorine from "$lib/components/tables/tableChlorine.svelte";
 import FormChlorine from "$lib/components/form/formChlorine.svelte";
+import { getModalStore, type ModalComponent, type ModalSettings } from "@skeletonlabs/skeleton";
+import OpenModalChlorine from "$lib/components/button/openModalChlorine.svelte";
+
+const modalStore = getModalStore();
+const modalComponent: ModalComponent = { ref: FormChlorine };
+
+const modal: ModalSettings = {
+	type: 'component',
+    component: modalComponent,
+	response: (data) => {
+      console.log('Form submitted:', data);
+    },
+};
+
+function openModal() {
+  modalStore.trigger(modal);
+}
+
 </script>
 
-<div class="container">
-    <h1>Cloro Libre</h1>
-    <TableChlorine />
-    <FormChlorine />
-</div>
+<h1>Cloro Libre</h1>
+<TableChlorine />
+<OpenModalChlorine on:openModal={openModal} />
