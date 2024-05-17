a72f88b7cff899729a4dcc3c4ff7f13b60df2f8f
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-17 17:07:35 -0300

    chore: Update import paths for Form and Table components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let data;
-	import Form from '$lib/componets/form.svelte';
-	import Table from '$lib/componets/table.svelte';
+	import Form from '$lib/components/form.svelte';
+	import Table from '$lib/components/table.svelte';
 </script>
 
 <Form />
