fc624d503dfd612b798c55ab4a65022ccb19afb1
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-17 20:48:35 -0300

    feat: Add open modal button to Chlorine page

---
+++ src/lib/components/button/openModalChlorine.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function openModal() {
+        dispatch('openModal');
+    }
+
+</script>
+
+<button type="button" on:click={openModal} class="btn variant-filled-primary">
+<span class="font-medium">Agregar medición</span>
+</button>
