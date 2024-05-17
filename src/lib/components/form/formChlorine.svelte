6604a9da35547203b7d8eeeaef629bc7be3d2fda
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-17 20:48:45 -0300

    feat: Refactor Chlorine form component and add modal functionality

--- src/lib/components/form/formChlorine.svelte
+++ src/lib/components/form/formChlorine.svelte
@@ -1,43 +1,71 @@
 <script lang="ts">
-      import type { WaterQuality } from '$lib/types';
-      import { freeChlorineAddItem } from '$lib/store/chlorineStore';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+  import { freeChlorineAddItem } from '$lib/store/chlorineStore';
 
-      let data: WaterQuality = {
-        date: '',
-        time: '',
-        location: '',
-        tap: 0,
-        freeChlorine: 0
-      };
+  const modalStore = getModalStore();
+  export let parent;
 
-      function handleSubmit(event: Event) {
-        event.preventDefault();
-        freeChlorineAddItem(data);
+  let data = {
+    date: '',
+    time: '',
+    location: '',
+    tap: 0,
+    freeChlorine: 0
+  };
 
-        data = {
-          date: '',
-          time: '',
-          location: '',
-          tap: 0,
-          freeChlorine: 0
-        };
+  function onFormSubmit() {
+    if ($modalStore[0].response) {
+      $modalStore[0].response(data); 
     }
+    freeChlorineAddItem(data);
+    modalStore.close();
+    resetForm();
+  }
 
+  function resetForm() {
+    data = {
+      date: '',
+      time: '',
+      location: '',
+      tap: 0,
+      freeChlorine: 0
+    };
+  }
+
+  const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+  const cHeader = 'text-2xl font-bold';
+  const cForm = 'border border-surface-500 p-4 space-y-4 rounded-container-token';
 </script>
 
-<form on:submit={handleSubmit}>
-    <label for="date">Dia</label>
-    <input type="date" id="date" name="date" class="input" bind:value={data.date} placeholder="Input" required>
-    <label for="time">Hora</label>
-    <input type="time" name="time" id="time" class="input" bind:value={data.time} required>
-    <label for="location">Sector</label>
-    <input type="text" id="location" name="location" class="input" bind:value={data.location} required>
-    <label for="tap">Grifo</label>
-    <input type="number" name="tap" id="tap" class="input" bind:value={data.tap} required>
-    <label for="freeChlorine">Cloro libre (ppm)</label>
-    <input type="number" name="freeChlorine" id="freeChlorine" class="input" bind:value={data.freeChlorine} min="0" max="2.50" step="0.01" required>
-    <button type="submit" class="btn variant-filled">Guardar</button>
-</form>
-
-
-
+{#if $modalStore[0]}
+  <div class="modal-example-form {cBase}">
+    <header class={cHeader}>Agregar nueva medición</header>
+    <article></article>
+    <form class="modal-form {cForm}" on:submit|preventDefault={onFormSubmit}>
+      <label class="label">
+        <span>Fecha</span>
+        <input type="date" id="date" name="date" class="input" bind:value={data.date} required>
+      </label>
+      <label class="label">
+        <span>Hora</span>
+        <input type="time" name="time" id="time" class="input" bind:value={data.time} required>
+      </label>
+      <label class="label">
+        <span>Sector</span>
+        <input type="text" id="location" name="location" class="input" bind:value={data.location} required>
+      </label>
+      <label class="label">
+        <span>Grifo</span>
+        <input type="number" name="tap" id="tap" class="input" bind:value={data.tap} required>
+      </label>
+      <label class="label">
+        <span>Cloro libre (ppm)</span>
+        <input type="number" name="freeChlorine" id="freeChlorine" class="input" bind:value={data.freeChlorine} min="0" max="2.50" step="0.01" required>
+      </label>
+      <footer class="modal-footer {parent.regionFooter}">
+        <button type="button" class="btn {parent.buttonNeutral}" on:click={parent.onClose}>Cancelar</button>
+        <button type="submit" class="btn {parent.buttonPositive}">Agregar</button>
+      </footer>
+    </form>
+  </div>
+{/if}
