6f6ab6103753c05ae1e6311ee6eab5b1e588b82e
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-17 17:43:31 -0300

    feat: Add Chlorine page layout and components for data entry and display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
 <script lang="ts">
-	export let data;
-	import Form from '$lib/components/form.svelte';
-	import Table from '$lib/components/table.svelte';
 </script>
-
-<Form />
-<Table />
---
+++ src/routes/chlorine/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import TableChlorine from "$lib/components/tables/tableChlorine.svelte";
+import FormChlorine from "$lib/components/form/formChlorine.svelte";
+</script>
+
+<div class="container">
+    <h1>Cloro Libre</h1>
+    <TableChlorine />
+    <FormChlorine />
+</div>
