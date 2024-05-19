a47e8e2944a0156aab2d8f184d88e5ded37a185e
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-19 17:49:32 -0300

    feat: Add lazy loading for images in tableChlorine.svelte

--- src/lib/components/tables/tableChlorine.svelte
+++ src/lib/components/tables/tableChlorine.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
     import { FreeChlorineStore } from "$lib/store/chlorineStore";
+	import { onMount } from "svelte";
+	import { fetchDataAndSetStore } from "$lib/utils/getData";
 
+	onMount(async () => {
+		await fetchDataAndSetStore('/api/chlorine', FreeChlorineStore);
+	});
+
+	const cCenter = 'text-center normal-case';
 </script>
 
 <div class="table-container">
@@ -8,27 +15,27 @@
 	<table class="table table-hover">
 		<thead>
 			<tr>
-				<th>Fecha</th>
-				<th>Hora</th>
-				<th>Sector</th>
-				<th>Grifo</th>
-                <th>Medicion ppm</th>
+				<th class="{cCenter}">Fecha</th>
+				<th class="{cCenter}">Hora</th>
+				<th class="{cCenter}">Sector</th>
+				<th class="{cCenter}">Grifo</th>
+                <th class="{cCenter}">Medición (ppm)</th>
 			</tr>
 		</thead>
 		<tbody>
 			{#each $FreeChlorineStore as row, i}
 				<tr>
-					<td>{row.date}</td>
-					<td>{row.time}</td>
-					<td>{row.location}</td>
-					<td>{row.tap}</td>
-                    <td>{row.freeChlorine}</td>
+					<td class="{cCenter}">{row.date}</td>
+					<td class="{cCenter}">{row.time}</td>
+					<td class="{cCenter}">{row.location}</td>
+					<td class="{cCenter}">{row.tap}</td>
+                    <td class="{cCenter}">{row.freeChlorine}</td>
 				</tr>
 			{/each}
 		</tbody>
 		<tfoot>
 			<tr>
-				<th colspan="3">Calculated Total Weight</th>
+				<!-- <th colspan="3">Calculated Total Weight</th> -->
 				<!-- <td>{totalWeight}</td> -->
 			</tr>
 		</tfoot>
