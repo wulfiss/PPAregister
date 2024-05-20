3f21f7bd4024850a84f17906c1b4b9becded7153
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-20 15:40:07 -0300

    feat: Add DownloadXLSX button component

---
+++ src/lib/components/button/DownloadXLSX.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { tableData } from "$lib/utils/getData";
+
+    function downloadXLSX() {
+		window.location.href = '/api/chlorine/download';
+	}
+
+</script>
+
+<button on:click={downloadXLSX} class="btn variant-filled-primary">Descarga XLSX</button>
