55f10bcca8cee12abe814a07d13a3cfac62c8b62
Julian <96155980+wulfiss@users.noreply.github.com>
2024-05-20 15:40:36 -0300

    refactor: Update fetchDataAndSetStore utility function to handle XLSX file download

--- src/lib/utils/getData.ts
+++ src/lib/utils/getData.ts
@@ -13,4 +13,30 @@ async function fetchDataAndSetStore<T>(endpoint: string, store: Writable<T>) {
     }
 }
 
-export { fetchDataAndSetStore };    
+/* async function tableData (endpoint: string) {
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data ${endpoint} ${response.statusText}`);
+        }
+        const data = await response.json();
+
+        const ws = XLSX.utils.json_to_sheet(data);
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "freechlorine");
+
+        const buf = XLSX.write(wb, { type: 'array', bookType: 'xlsx'});
+        const blob = new Blob([buf], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+
+        return new Response(blob,{
+            headers: {
+                'Content-Disposition': 'attachment; filename="freechlorine.xlsx"'
+        }
+    })
+    } catch (error) {
+        console.error('Error fetching and setting data:', error);
+    }
+}
+ */
+
+export { fetchDataAndSetStore };  
